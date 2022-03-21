4cbd80b0fe4571653b6a2e8de1ff396b6eeb0ea2
Kanchan Kaushal <kaushalkanchan238@gmail.com>
2022-03-21 14:07:11 +0530

    first commit

---
+++ EventsAndDelegatesPractice/EventsAndDelegatesPractice/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegatesPractice
+{
+    internal class Calculator
+    {
+
+        public event Action<object, CalculatorEventArgs> Calculate;
+
+        public void RaiseEvent(string name)
+        {
+            Calculate?.Invoke(this, new CalculatorEventArgs { Name = name });
+        }
+        //public delegate int Calculate(int input);
+        public int Execute(Func<int, int> calculate, Action<int> print, int input)
+        {
+            var result = calculate(input);
+            print(result);
+            return result;
+        }
+    }
+}
+// event is associated with a delegate
+// when event is raised the delegate is called back
+// best practice:
+// the first one is object representing the instance that raised the event
+// the second one is a type representing event arguments
---
+++ EventsAndDelegatesPractice/EventsAndDelegatesPractice/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegatesPractice
+{
+    internal class Class1
+    {
+       // static int Square(int input) => input * input;
+
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+
+            var response = calculator.Execute((x) => x * x, Console.WriteLine, 5);
+            calculator.Calculate += Calculator_Calculate;
+            calculator.RaiseEvent("Kanchan");
+            //Console.WriteLine("Result: "+response);
+        }
+        public static void Calculator_Calculate(object arg1, CalculatorEventArgs arg2)
+        {
+            Console.WriteLine(arg2.Name);
+        }
+    }
+}
